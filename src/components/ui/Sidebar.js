584e91c8f415f4704b5e5136b49cda3a2f025bbd
manci <gmancillapaz@gmail.com>
2021-03-01 15:38:38 -0500

    poner activeclassname en navlink beta 6 y end para activar en click

--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
       <div className="p-6">
         <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">RestaurantApp</p>  
+        <p className="mt-3 text-gray-600">Administra tu restaurante en las gisuientes opciones:</p>
+        <nav>
+          <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" end to="/">Órdenes</NavLink>
+          <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" end to="/menu">Menú</NavLink>
+        </nav>
       </div>
     </div>
   );
