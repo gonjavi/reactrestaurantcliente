5774a205b00801d77135e55102d7816f101e7cc1
manci <gmancillapaz@gmail.com>
2021-03-07 21:38:10 -0500

    downgrade node to use countdown

--- src/components/ui/Orden.js
+++ src/components/ui/Orden.js
@@ -20,6 +20,19 @@ const Orden = ({ orden }) => {
     }
   }
 
+  // completa estado de una orden
+  const completarOrden = id => {
+    try {
+      firebase.db.collection('ordenes')
+          .doc(id)
+          .update({
+            completado: true
+          })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="sm:w-1/2 lg:w-1/3 px-2 mb-4">
       <div className="p-3 shadow-md bg-white">
@@ -61,6 +74,16 @@ const Orden = ({ orden }) => {
           <p className="text-gray-700">Tiempo de entrega
             <span className="font-bold">{orden.tiempoentrega}  Minutos</span>
           </p>
+        )}
+
+        { !orden.completado && orden.tiempoentrega > 0 && (
+          <button
+            type="button"
+            className="bg-blue-800 hover:bg-blue-700 w-full mt-5 p-2 text-white uppercase font-bold"
+            onClick={() => completarOrden(orden.id)}
+          >
+            Marcar como lista
+          </button>
         )}
       </div>
     </div>
